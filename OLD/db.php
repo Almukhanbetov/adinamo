49a4bf0010b833c3011457dc16bb5d3d1384f1c5
Almukhanbetov <almuko.m@gmail.com>
2019-02-06 16:05:15 +0600

    Revert "Ссылка на Главную картинки"
    This reverts commit 3d4364c12c502223a10ca6385f7fe16777553d2f.

--- OLD/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=p-15492_dinamo',
-    'username' => 'root',
-    'password' => 'Zxcvbnm123',
-    'charset' => 'utf8',
-];
