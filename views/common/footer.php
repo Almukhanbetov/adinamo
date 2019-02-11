d1ea4f21c267e181020b33a4a626602949d0c6ec
Almukhanbetov <almuko.m@gmail.com>
2019-02-11 14:08:52 +0600

    Revert "Убрал теги"
    This reverts commit ee829a1355fb94f35f5af01f8484eb9eb0a0337f.

--- views/common/footer.php
+++ views/common/footer.php
@@ -27,23 +27,20 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="newsletter-area">
-                                <h3>Написать нам</h3>
-                                <div class="input-group stylish-input-group">
-                                    <input type="text" class="form-control" placeholder="E-mail . . .">
-                                    <span class="input-group-addon">
-							                        <button type="submit">
-							                            <i class="fa fa-paper-plane" aria-hidden="true"></i>
-							                        </button>
-							                    </span>
-                                
-                            </div>
-				<ul class="footer-social">
-                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="footer-box">
+                            <h3>Теги продуктов</h3>
+                            <ul class="tag-list">
+                                <li><a href="#">Акваспорт</a></li>
+                                <li><a href="#">Бокс и единоборства</a></li>
+                                <li><a href="#">Велосипеды и Аксессуары</a></li>
+                                <li><a href="#">Зимние виды спорта</a></li>
+                                <li><a href="#">Командные виды спорта</a></li>
+                                <li><a href="#">Кубки и медали</a></li>
+
                             </ul>
+                        </div>
+                    </div>
                     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                         <div class="footer-box">
                             <h3>Контакты</h3>
@@ -52,10 +49,26 @@
 			       <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8 (777) 583 77 99</p>
                                <p>Адрес: Кунаева, 83 Медеуский район, Алматы</p>
 			       <p>Почта: info@dinamo.kz &nbsp;&nbsp;&nbsp; sales@dinamo.kz</p>
-				</div>
+
 
                             
-                           
+                            <div class="newsletter-area">
+                                <h3>Написать нам</h3>
+                                <div class="input-group stylish-input-group">
+                                    <input type="text" class="form-control" placeholder="E-mail . . .">
+                                    <span class="input-group-addon">
+							                        <button type="submit">
+							                            <i class="fa fa-paper-plane" aria-hidden="true"></i>
+							                        </button>
+							                    </span>
+                                </div>
+                            </div>
+				<ul class="footer-social">
+                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
