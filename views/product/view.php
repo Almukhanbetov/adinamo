f84c317d01218755743e7e4a13514dcadecf1453
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-19 10:14:29 +0600

    php
    php

--- views/product/view.php
+++ views/product/view.php
@@ -28,7 +28,12 @@ use yii\helpers\Url;
                     <p class="price"><?=$product->price?>₸</p>
                     <p><?=$product->short_content?></p>
                     <div class="product-details-content">
-                        <p><span>Номер товара:&nbsp;&nbsp;&nbsp;</span><?=$product->code?></p>
+                        <p><span>Номер товара:&nbsp;&nbsp;&nbsp;</span><?php
+                            if ($product->code? > 0) {
+                                echo 'В наличии';
+                            } else {
+                                 echo 'Не в наличии';
+                        ?></p>
                         <p><span>На складе:&nbsp;&nbsp;&nbsp;</span><?=$product->status?></p>
                         <p><span>Категория:&nbsp;&nbsp;&nbsp;</span><?=$product->category_id?></p>
                     </div>
