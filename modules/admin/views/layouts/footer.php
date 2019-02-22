44f75854f48296e801bfd24ac038bfb2d1e80aa2
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 16:14:19 +0600

    Footer
    Footer

--- modules/admin/views/layouts/footer.php
+++ modules/admin/views/layouts/footer.php
@@ -1,6 +1,6 @@
 	  <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 1.5.7
+          <b>Beta Version</b> 0.1.5.7
         </div>
         <strong>©compnet.kz<a href="http://compnet.kz" target="_blank">ТОО "Complex Services"</a>.</strong> All rights reserved.
       </footer>
