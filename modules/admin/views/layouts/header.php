896cd56d6f22d8515b3619e9bd4f75d13fbdca65
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 16:11:30 +0600

    Menu
    Menu

--- modules/admin/views/layouts/header.php
+++ modules/admin/views/layouts/header.php
@@ -2,13 +2,10 @@
 use yii\helpers\Html;
 ?>
 <header class="main-header">
-        <!-- Logo -->
-        <a href="index2.html" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b><?=$title?></b></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b><?=$title?></b> Administrator</span>
-        </a>
+          <span class="logo-lg"><b>Меню</b> Administrator</span>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
           <!-- Sidebar toggle button-->
