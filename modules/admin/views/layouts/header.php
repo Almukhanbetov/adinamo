c03b08537e8ee2ed463c9896758a07d92d55aea4
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 16:36:31 +0600

    Исправил
    Исправил

--- modules/admin/views/layouts/header.php
+++ modules/admin/views/layouts/header.php
@@ -2,10 +2,6 @@
 use yii\helpers\Html;
 ?>
 <header class="main-header">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-         	 <span class="logo-mini"><b><?=title?></b></span>
-          <!-- logo for regular state and mobile devices -->
-         	 <span class="logo-lg"><b>Меню</b> Administrator</span>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
           <!-- Sidebar toggle button-->
