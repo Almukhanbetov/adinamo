3f1effd0d4b1de64cfee7644402a2fb970fa29d5
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 16:22:24 +0600

    Поставил
    Поставил

--- modules/layouts/footer.php
+++ modules/layouts/footer.php
@@ -1,6 +1,6 @@
 	  <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 2.3.0
+          <b>Beta Version</b> 0.1.5.7
         </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+        <strong>©compnet.kz<a href="http://compnet.kz" target="_blank">ТОО "Complex Services"</a>.</strong> All rights reserved.
       </footer>
