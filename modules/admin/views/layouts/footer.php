0aeaa6e80cc444730acf18268ecfc93284cc951f
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 17:45:55 +0600

    Сменил версию
    Сменил версию

--- modules/admin/views/layouts/footer.php
+++ modules/admin/views/layouts/footer.php
@@ -1,6 +1,6 @@
 	  <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Beta Version</b> 0.1.5.7
+          <b>Beta Version</b> 0.1.8.4
         </div>
-        <strong>©Сompnet.kz<a href="http://compnet.kz" target="_blank">ТОО "Complex Services"</a>.</strong> All rights reserved.
+        <strong>©Сompnet.kz<a href="http://compnet.kz" target="_blank">&nbsp;&nbsp;ТОО "Complex Services"</a>.</strong> All rights reserved.
       </footer>
