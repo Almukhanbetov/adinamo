1741f72d737b72eba4a99153b26d139714b1edb5
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 17:18:20 +0600

    View
    View

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -63,41 +63,41 @@ AppAsset::register($this);
                         </div>
                         <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
                             <div class="inner-product-details-right">
-                                <h3>Product Title Here</h3>
-                                <ul>
-                                    <li><i aria-hidden="true" class="fa fa-star"></i></li>
-                                    <li><i aria-hidden="true" class="fa fa-star"></i></li>
-                                    <li><i aria-hidden="true" class="fa fa-star"></i></li>
-                                    <li><i aria-hidden="true" class="fa fa-star"></i></li>
-                                    <li><i aria-hidden="true" class="fa fa-star"></i></li>
-                                </ul>
-                                <p class="price">$50.00</p>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tinc amet risus consectetur, non consectetur nisl finibus. Ut ac eros quis mi volutpat cursus vel non risus.</p>
-                                <div class="product-details-content">
-                                    <p><span>SKU:</span> 0010</p>
-                                    <p><span>Availability:</span> In stock</p>
-                                    <p><span>Category:</span> Demo Products</p>
+                        <h3><?=$product->name?></h3>
+                        <ul>
+                            <li><i aria-hidden="true" class="fa fa-star"></i></li>
+                            <li><i aria-hidden="true" class="fa fa-star"></i></li>
+                            <li><i aria-hidden="true" class="fa fa-star"></i></li>
+                            <li><i aria-hidden="true" class="fa fa-star"></i></li>
+                            <li><i aria-hidden="true" class="fa fa-star"></i></li>
+                        </ul>
+                        <p class="price"><?=$product->price?>₸</p>
+                        <p><?=$product->content?></p>
+                        <p><?=$product->short_content?></p>
+                        <div class="product-details-content">
+                            <p><span>Номер товара:&nbsp;&nbsp;&nbsp;</span><?=$product->code?></p>
+                            <p><span>На складе:&nbsp;&nbsp;&nbsp;</span><?=$product->status?></p>
+                            <p><span>Категория:&nbsp;&nbsp;&nbsp;</span><?=$product->category_id?></p>
+                        </div>
+                        <ul class="product-details-social">
+                            <li>Поделиться:</li>
+                            <li><a href="https://www.facebook.com/sharer/sharer.php?u=http://192.168.2.69/<?=$product->id?>"><i aria-hidden="true" class="fa fa-facebook"></i></a></li>
+                            <li><a href="http://vk.com/share.php?url=http://192.168.2.69/<?=$product->id?>"><i aria-hidden="true" class="fa fa-vk"></i></a></li>
+                            <li><a href="http://www.odnoklassniki.ru/share.php?url=http://192.168.2.69/<?=$product->id?>"><i aria-hidden="true" class="fa fa-odnoklassniki"></i></a></li>
+                        </ul>
+                                          <ul class="inner-product-details-cart">
+                            <li><a href="<?=Url::to(['cart/add','id'=>$product->id])?>" data-id="<?=$product->id?>" class="add-to-cart">В корзину</a></li>
+                            <li>
+                                <div class="input-group quantity-holder" id="quantity-holder">
+                                    <input type="text" placeholder="1" value="1" class="form-control quantity-input" name="quantity">
+                                    <div class="input-group-btn-vertical">
+                                        <button type="button" class="btn btn-default quantity-plus"><i aria-hidden="true" class="fa fa-plus"></i></button>
+                                        <button type="button" class="btn btn-default quantity-minus"><i aria-hidden="true" class="fa fa-minus"></i></button>
+                                    </div>
                                 </div>
-                                <ul class="product-details-social">
-                                    <li>Share:</li>
-                                    <li><a href="#"><i aria-hidden="true" class="fa fa-facebook"></i></a></li>
-                                    <li><a href="#"><i aria-hidden="true" class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#"><i aria-hidden="true" class="fa fa-linkedin"></i></a></li>
-                                    <li><a href="#"><i aria-hidden="true" class="fa fa-pinterest"></i></a></li>
-                                </ul>
-                                <ul class="inner-product-details-cart">
-                                    <li><a href="#">Add To Cart</a></li>
-                                    <li>
-                                        <div class="input-group quantity-holder" id="quantity-holder">
-                                            <input type="text" placeholder="1" value="1" class="form-control quantity-input" name="quantity">
-                                            <div class="input-group-btn-vertical">
-                                                <button type="button" class="btn btn-default quantity-plus"><i aria-hidden="true" class="fa fa-plus"></i></button>
-                                                <button type="button" class="btn btn-default quantity-minus"><i aria-hidden="true" class="fa fa-minus"></i></button>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a></li>
-                                </ul>
+                            </li>
+                            <li><a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a></li>
+                        </ul>
                             </div>
                         </div>
                     </div>
