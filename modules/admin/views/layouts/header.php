059b7f5958692f81cef346c3a5fcc5411538cba6
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 16:02:47 +0600

    Admin-D
    Admin-D

--- modules/admin/views/layouts/header.php
+++ modules/admin/views/layouts/header.php
@@ -208,15 +208,15 @@ use yii\helpers\Html;
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
 					<?= Html::img('@web/img/user2-160x160.jpg', ['class' => 'user-image', 'alt'=>'User Image']) ?>
-                  <span class="hidden-xs">Alexander Pierce</span>
+                  <span class="hidden-xs">Almukhanbetov Mukhtar</span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
                     <?= Html::img('@web/img/user2-160x160.jpg', ['class' => 'img-circle', 'alt'=>'User Image']) ?>
                     <p>
-                      Alexander Pierce - Web Developer
-                      <small>Member since Nov. 2012</small>
+                      Almukhanbetov Mukhtar - Back-end Developer
+                      <small>Member since Nov. 2019</small>
                     </p>
                   </li>
                   <!-- Menu Body -->
