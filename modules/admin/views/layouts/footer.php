a59803e6c768016d83aeea67b819fcf92250192c
Lexacamp <47312834+Lexacamp@users.noreply.github.com>
2019-02-22 15:32:33 +0600

    Изменил Footer
    Изменил Footer

--- modules/admin/views/layouts/footer.php
+++ modules/admin/views/layouts/footer.php
@@ -2,5 +2,5 @@
         <div class="pull-right hidden-xs">
           <b>Version</b> 2.3.0
         </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+        <strong>©compnet.kz<a href="http://compnet.kz" target="_blank">ТОО "Complex Services"</a>.</strong> All rights reserved.
       </footer>
